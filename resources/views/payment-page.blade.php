cdadf63920abdb5cdf01fa983b1178b9cd87a5a1
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 03:36:19 +0800

    Update payment-page.blade.php

--- resources/views/payment-page.blade.php
+++ resources/views/payment-page.blade.php
@@ -144,8 +144,12 @@ include "header.php"
     </div>
   </div>
 </section>
-
-<?php
-include "footer.php"
-?>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/jquery.slicknav.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
 <script src="https://kit.fontawesome.com/897a3f2775.js" crossorigin="anonymous"></script>
