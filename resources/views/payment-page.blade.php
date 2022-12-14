acfabe802b9c6f8844ea653b8b9046da8fa45b2e
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 03:11:49 +0800

    a

--- resources/views/payment-page.blade.php
+++ resources/views/payment-page.blade.php
@@ -1,142 +1,56 @@
-<section style="background-color: #eee;">
-  <div class="container py-5">
-    <div class="card">
-      <div class="card-body">
-        <div class="row d-flex justify-content-center pb-5">
-          <div class="col-md-7 col-xl-5 mb-4 mb-md-0">
-            <div class="py-4 d-flex flex-row">
-              <h5><span class="far fa-check-square pe-2"></span><b>ELIGIBLE</b> |</h5>
-              <span class="ps-2">Pay</span>
-            </div>
-            <h4 class="text-success">$85.00</h4>
-            <h4>Diabetes Pump & Supplies</h4>
-            <div class="d-flex pt-2">
-              <div>
-                <p>
-                  <b>Insurance Responsibility <span class="text-success">$71.76</span></b>
-                </p>
-              </div>
-              <div class="ms-auto">
-                <p class="text-primary">
-                  <i class="fas fa-plus-circle text-primary pe-1"></i>Add insurance card
-                </p>
-              </div>
-            </div>
-            <p>
-              Insurance claims and all necessary dependencies will be submitted to your
-              insurer for the coverred portion of this order
-            </p>
-            <div class="rounded d-flex" style="background-color: #f8f9fa;">
-              <div class="p-2">Aetna-Open Access</div>
-              <div class="ms-auto p-2">OAP</div>
-            </div>
-            <hr />
-            <div class="pt-2">
-              <div class="d-flex pb-2">
-                <div>
-                  <p>
-                    <b>Patient Balance <span class="text-success">$13.24</span></b>
-                  </p>
-                </div>
-                <div class="ms-auto">
-                  <p class="text-primary">
-                    <i class="fas fa-plus-circle text-primary pe-1"></i>Add payment card
-                  </p>
-                </div>
-              </div>
-              <p>
-                This is an estimate for the portion of your order (not covered by
-                insurance) due today . once insurance finalizes their review refunds
-                and/or balances will reconcile automatically.
-              </p>
-              <form class="pb-3">
-                <div class="d-flex flex-row pb-3">
-                  <div class="d-flex align-items-center pe-2">
-                    <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel1"
-                      value="" aria-label="..." checked />
-                  </div>
-                  <div class="rounded border d-flex w-100 p-3 align-items-center">
-                    <p class="mb-0">
-                      <i class="fab fa-cc-visa fa-lg text-primary pe-2"></i>Visa Debit
-                      Card
-                    </p>
-                    <div class="ms-auto">************3456</div>
-                  </div>
-                </div>
-
-                <div class="d-flex flex-row">
-                  <div class="d-flex align-items-center pe-2">
-                    <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel2"
-                      value="" aria-label="..." />
-                  </div>
-                  <div class="rounded border d-flex w-100 p-3 align-items-center">
-                    <p class="mb-0">
-                      <i class="fab fa-cc-mastercard fa-lg text-dark pe-2"></i>Mastercard
-                      Office
-                    </p>
-                    <div class="ms-auto">************1038</div>
-                  </div>
-                </div>
-              </form>
-              <input type="button" value="Proceed to payment" class="btn btn-primary btn-block btn-lg" />
-            </div>
-          </div>
-
-          <div class="col-md-5 col-xl-4 offset-xl-1">
-            <div class="py-4 d-flex justify-content-end">
-              <h6><a href="#!">Cancel and return to website</a></h6>
-            </div>
-            <div class="rounded d-flex flex-column p-2" style="background-color: #f8f9fa;">
-              <div class="p-2 me-3">
-                <h4>Order Recap</h4>
-              </div>
-              <div class="p-2 d-flex">
-                <div class="col-8">Contracted Price</div>
-                <div class="ms-auto">$186.76</div>
-              </div>
-              <div class="p-2 d-flex">
-                <div class="col-8">Amount toward deductible</div>
-                <div class="ms-auto">$0.00</div>
-              </div>
-              <div class="p-2 d-flex">
-                <div class="col-8">Coinsurance( 0% )</div>
-                <div class="ms-auto">+ $0.00</div>
-              </div>
-              <div class="p-2 d-flex">
-                <div class="col-8">Copayment</div>
-                <div class="ms-auto">+ $40.00</div>
-              </div>
-              <div class="border-top px-2 mx-2"></div>
-              <div class="p-2 d-flex pt-3">
-                <div class="col-8">Total Deductible, Coinsurance, and Copay</div>
-                <div class="ms-auto">$40.00</div>
-              </div>
-              <div class="p-2 d-flex">
-                <div class="col-8">
-                  Maximum out-of-pocket on Insurance Policy (not reached)
-                </div>
-                <div class="ms-auto">$6500.00</div>
-              </div>
-              <div class="border-top px-2 mx-2"></div>
-              <div class="p-2 d-flex pt-3">
-                <div class="col-8">Insurance Responsibility</div>
-                <div class="ms-auto"><b>$71.76</b></div>
-              </div>
-              <div class="p-2 d-flex">
-                <div class="col-8">
-                  Patient Balance <span class="fa fa-question-circle text-dark"></span>
-                </div>
-                <div class="ms-auto"><b>$71.76</b></div>
-              </div>
-              <div class="border-top px-2 mx-2"></div>
-              <div class="p-2 d-flex pt-3">
-                <div class="col-8"><b>Total</b></div>
-                <div class="ms-auto"><b class="text-success">$85.00</b></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+<div class="container mt-3 mb-3">
+    <div class="d-flex justify-content-between align-items-center mb-5">
+        <div class="d-flex flex-row align-items-center">
+            <h4 class="text-uppercase mt-1">Eligible</h4> <span class="ml-2">Pay</span>
+        </div> <a href="#" class="cancel com-color">Cancel and return to website</a>
     </div>
-  </div>
-</section>
+    <div class="row">
+        <div class="col-md-6">
+            <h5 class="mb-0 text-success">$85.00</h5>
+            <h5 class="mb-3">Diabities Pump & Supplies</h5>
+            <div class="about">
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-row mt-1">
+                        <h6>Insurance Responsibility</h6>
+                        <h6 class="text-success font-weight-bold ml-1">$71.76</h6>
+                    </div>
+                    <div class="d-flex flex-row align-items-center com-color"> <i class="fa fa-plus-circle"></i> <span class="ml-1">Add Insurer card</span> </div>
+                </div>
+                <p>Insurance claim and all neccessary dependencies will be submitted to your insurer for the covered portion of this order.</p>
+                <div class="p-2 d-flex justify-content-between bg-pay align-items-center"> <span>Aetna - Open Access</span> <span>OAP</span> </div>
+                <hr>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex flex-row mt-1">
+                        <h6>Patient Balance</h6>
+                        <h6 class="text-success font-weight-bold ml-1">$13.24</h6>
+                    </div>
+                    <div class="d-flex flex-row align-items-center com-color"> <i class="fa fa-plus-circle"></i> <span class="ml-1">Add Payment card</span> </div>
+                </div>
+                <p>Insurance claim and all neccessary dependencies will be submitted to your insurer for the covered portion of this order.</p>
+                <div class="d-flex flex-column"> <label class="radio"> <input type="radio" name="gender" value="MALE" checked>
+                        <div class="d-flex justify-content-between"> <span>VISA</span> <span>**** 5645</span> </div>
+                    </label> <label class="radio"> <input type="radio" name="gender" value="FEMALE">
+                        <div class="d-flex justify-content-between"> <span>MASTER CARD</span> <span>**** 5069</span> </div>
+                    </label> </div>
+                <div class="buttons"> <button class="btn btn-success btn-block">Proceed to payment</button> </div>
+            </div>
+        </div>
+        <div class="col-md-2"> </div>
+        <div class="col-md-4">
+            <div class="bg-pay p-3"> <span class="font-weight-bold">Order Recap</span>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">contracted Price</span> <span>$186.86</span> </div>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">Amount Deductible</span> <span>$0.0</span> </div>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">Coinsurance(0%)</span> <span>+ $0.0</span> </div>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">Copayment </span> <span>+ 40.00</span> </div>
+                <hr>
+                <div class="d-flex justify-content-between mt-2"> <span class="lh-16 fw-500">Total Deductible,<br> Coinsurance and copay </span> <span>$40.00</span> </div>
+                <div class="d-flex justify-content-between mt-2"> <span class="lh-16 fw-500">Maximum out-of-pocket <br> on insurance policy</span> <span>$40.00</span> </div>
+                <hr>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">Insurance Responsibility </span> <span>$71.76</span> </div>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">Patient Balance </span> <span>$13.24</span> </div>
+                <hr>
+                <div class="d-flex justify-content-between mt-2"> <span class="fw-500">Total </span> <span class="text-success">$85.00</span> </div>
+            </div>
+        </div>
+    </div>
+</div>
