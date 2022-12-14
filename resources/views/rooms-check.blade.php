3c321737d71d02525d92a15d7509e46911dec940
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 01:28:57 +0800

    Create rooms-check.blade.php

---
+++ resources/views/rooms-check.blade.php
@@ -0,0 +1,146 @@
+<?php
+include "header.php"
+?>
+
+
+<!-- About Us Page Section Begin -->
+<section class="aboutus-page-section spad">
+    <div class="container">
+        <div class="about-page-text">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="ap-title">
+                    <div class="card">
+  <img src="..." class="card-img-top" alt="...">
+
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" aria-hidden="true">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+      <span class="placeholder col-6"></span>
+    </h5>
+    <p class="card-text placeholder-glow">
+      <span class="placeholder col-7"></span>
+      <span class="placeholder col-4"></span>
+      <span class="placeholder col-4"></span>
+      <span class="placeholder col-6"></span>
+      <span class="placeholder col-8"></span>
+    </p>
+    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+  </div>
+</div>
+                    </div>
+                </div>
+                <div class="col-lg-5 offset-lg-1">
+                    <ul class="ap-services">
+                        <li><i class="icon_check"></i> 20% Off On Accommodation.</li>
+                        <li><i class="icon_check"></i> 5 star Breakfast</li>
+                        <li><i class="icon_check"></i> 5 Pcs Laundry Per Day</li>
+                        <li><i class="icon_check"></i> Free Wifi with speed up to 1 gb/sec per room.</li>
+                        <li><i class="icon_check"></i> Discount 20% On F&B</li>
+                        <li><i class="icon_check"></i> Gym free to use 24 hour</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="about-page-services">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="ap-service-item set-bg" data-setbg="img/about/about-p1.jpg">
+                        <div class="api-text">
+                            <h3>Restaurants Services</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="ap-service-item set-bg" data-setbg="img/about/about-p2.jpg">
+                        <div class="api-text">
+                            <h3>Gym & Laundry</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="ap-service-item set-bg" data-setbg="img/about/about-p3.jpg">
+                        <div class="api-text">
+                            <h3>Event & Party</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Us Page Section End -->
+
+<!-- Video Section Begin
+<section class="video-section set-bg" data-setbg="img/video-bg.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="video-text">
+                    <h2>Discover Our Hotel & Services.</h2>
+                    <p>It S Hurricane Season But We Are Visiting Hilton Head Island</p>
+                    <a href="https://www.youtube.com/watch?v=EzKkl64rRbM" class="play-btn video-popup"><img src="img/play.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- Video Section End -->
+
+<!-- Gallery Section Begin -->
+<section class="gallery-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <span>Our Gallery</span>
+                    <h2>Discover Our Work</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="gallery-item set-bg" data-setbg="img/gallery/gallery-1.jpg">
+                    <div class="gi-text">
+                        <h3>Room Luxury</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="gallery-item set-bg" data-setbg="img/gallery/gallery-3.jpg">
+                            <div class="gi-text">
+                                <h3>Event & Party</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="gallery-item set-bg" data-setbg="img/gallery/gallery-4.jpg">
+                            <div class="gi-text">
+                                <h3>Facilities</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="gallery-item large-item set-bg" data-setbg="img/gallery/gallery-2.jpg">
+                    <div class="gi-text">
+                        <h3>Our Hotel</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Gallery Section End -->
+<?php
+include "footer.php"
+?>
