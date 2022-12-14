0ac03fce1b268cf79ddc39844c48ebf30419639f
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 02:29:04 +0800

    Update my-booking.blade.php

--- resources/views/my-booking.blade.php
+++ resources/views/my-booking.blade.php
@@ -23,20 +23,18 @@ include "header.php"
                                         <p class="card-text">Price: Rp.450.000/Night</p>
                                         <!-- code untuk collapse -->
                                         <p>
-                                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
-                                                data-bs-target="#collapseWidthExample" aria-expanded="false"
-                                                aria-controls="collapseWidthExample">
-                                                Lihat detail informasi
-                                            </button>
-                                        </p>
-                                        <div style="min-height: 120px;">
-                                            <div class="collapse collapse-horizontal" id="collapseWidthExample">
-                                                <div class="card card-body" style="width: 300px;">
-                                                    This is some placeholder content for a horizontal collapse. It's
-                                                    hidden by default and shown when triggered.
-                                                </div>
-                                            </div>
-                                        </div>
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Link with href
+  </a>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-bs-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+  </div>
+</div>
                                         <!-- tutup code collapse -->
                                     </div>
                                 </div>
