9a46366c4be0709361a1ac08f5f4b552b7553cd3
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 02:20:28 +0800

    Update my-booking.blade.php

--- resources/views/my-booking.blade.php
+++ resources/views/my-booking.blade.php
@@ -11,14 +11,34 @@ include "header.php"
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px" height="200px">
+                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px"
+                                            height="200px">
                                     </div>
                                     <div class="col-6">
-                                    <!-- < $tipekamar&$nokamar> -->
+                                        <!-- < $tipekamar&$nokamar> -->
                                         <h5 class="card-title">Reguler Suite #1</h5>
-                                        <p class="card-text">Capacity:	2 person</p>
-                                        <p class="card-text">Services:	Wifi, Television, Bathroom, Dining/Work Table, Wardrobe, Free Breakfast</p>
-                                        <p class="card-text">Price:	    Rp.450.000/Night</p>
+                                        <p class="card-text">Capacity: 2 person</p>
+                                        <p class="card-text">Services: Wifi, Television, Bathroom, Dining/Work Table,
+                                            Wardrobe, Free Breakfast</p>
+                                        <p class="card-text">Price: Rp.450.000/Night</p>
+                                        <p>
+                                            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample"
+                                                role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                Link with href
+                                            </a>
+                                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                                                data-bs-target="#collapseExample" aria-expanded="false"
+                                                aria-controls="collapseExample">
+                                                Button with data-bs-target
+                                            </button>
+                                        </p>
+                                        <div class="collapse" id="collapseExample">
+                                            <div class="card card-body">
+                                                Some placeholder content for the collapse component. This panel is
+                                                hidden by default but revealed when the user activates the relevant
+                                                trigger.
+                                            </div>
+                                        </div>
                                         <a href="rooms-details" class="btn btn-primary">Book Now</a>
                                     </div>
                                 </div>
@@ -27,14 +47,16 @@ include "header.php"
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px" height="200px">
+                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px"
+                                            height="200px">
                                     </div>
                                     <div class="col-6">
-                                    <!-- < $tipekamar&$nokamar> -->
+                                        <!-- < $tipekamar&$nokamar> -->
                                         <h5 class="card-title">Reguler Suite #1</h5>
-                                        <p class="card-text">Capacity:	2 person</p>
-                                        <p class="card-text">Services:	Wifi, Television, Bathroom, Dining/Work Table, Wardrobe, Free Breakfast</p>
-                                        <p class="card-text">Price:	    Rp.450.000/Night</p>
+                                        <p class="card-text">Capacity: 2 person</p>
+                                        <p class="card-text">Services: Wifi, Television, Bathroom, Dining/Work Table,
+                                            Wardrobe, Free Breakfast</p>
+                                        <p class="card-text">Price: Rp.450.000/Night</p>
                                         <a href="rooms-details" class="btn btn-primary">Book Now</a>
                                     </div>
                                 </div>
@@ -43,14 +65,16 @@ include "header.php"
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px" height="200px">
+                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px"
+                                            height="200px">
                                     </div>
                                     <div class="col-6">
-                                    <!-- < $tipekamar&$nokamar> -->
+                                        <!-- < $tipekamar&$nokamar> -->
                                         <h5 class="card-title">Reguler Suite #1</h5>
-                                        <p class="card-text">Capacity:	2 person</p>
-                                        <p class="card-text">Services:	Wifi, Television, Bathroom, Dining/Work Table, Wardrobe, Free Breakfast</p>
-                                        <p class="card-text">Price:	    Rp.450.000/Night</p>
+                                        <p class="card-text">Capacity: 2 person</p>
+                                        <p class="card-text">Services: Wifi, Television, Bathroom, Dining/Work Table,
+                                            Wardrobe, Free Breakfast</p>
+                                        <p class="card-text">Price: Rp.450.000/Night</p>
                                         <a href="rooms-details" class="btn btn-primary">Book Now</a>
                                     </div>
                                 </div>
