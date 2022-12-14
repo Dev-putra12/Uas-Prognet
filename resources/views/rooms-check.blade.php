d81e2fb6fb33e4832e9c83c6a3854290173b0b89
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 01:44:25 +0800

    Update rooms-check.blade.php

--- resources/views/rooms-check.blade.php
+++ resources/views/rooms-check.blade.php
@@ -10,77 +10,77 @@ include "header.php"
             <div class="row">
                 <div class="col-lg-10">
                     <div class="ap-title">
-                    <div class="card">
-  <div class="card-body">
-<div class="row">
-    <div class="col-4">  <img src="img/room/room-details.jpg" alt="..." width="200px"> </div>
-    <div class="col-6"><h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a></div>
-</div>
-  
-    </div>
-<div class="col-8">
-    
-    </div>
-  </div>
-</div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="200px">
+                                    </div>
+                                    <div class="col-6">
+                                        <h5 class="card-title"><?= $tipekamar&$nokamar?></h5>
+                                        <p class="card-text">Capacity:	2 person</p>
+                                        <p class="card-text">Services:	Wifi, Television, Bathroom, Dining/Work Table, Wardrobe, Free Breakfast</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-<div class="card" aria-hidden="true">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title placeholder-glow">
-      <span class="placeholder col-6"></span>
-    </h5>
-    <p class="card-text placeholder-glow">
-      <span class="placeholder col-7"></span>
-      <span class="placeholder col-4"></span>
-      <span class="placeholder col-4"></span>
-      <span class="placeholder col-6"></span>
-      <span class="placeholder col-8"></span>
-    </p>
-    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
-  </div>
-</div>
-                    </div>
-                </div>
-                <div class="col-lg-5 offset-lg-1">
-                    <ul class="ap-services">
-                        <li><i class="icon_check"></i> 20% Off On Accommodation.</li>
-                        <li><i class="icon_check"></i> 5 star Breakfast</li>
-                        <li><i class="icon_check"></i> 5 Pcs Laundry Per Day</li>
-                        <li><i class="icon_check"></i> Free Wifi with speed up to 1 gb/sec per room.</li>
-                        <li><i class="icon_check"></i> Discount 20% On F&B</li>
-                        <li><i class="icon_check"></i> Gym free to use 24 hour</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="about-page-services">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="ap-service-item set-bg" data-setbg="img/about/about-p1.jpg">
-                        <div class="api-text">
-                            <h3>Restaurants Services</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="ap-service-item set-bg" data-setbg="img/about/about-p2.jpg">
-                        <div class="api-text">
-                            <h3>Gym & Laundry</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="ap-service-item set-bg" data-setbg="img/about/about-p3.jpg">
-                        <div class="api-text">
-                            <h3>Event & Party</h3>
+                    <div class="card" aria-hidden="true">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                            <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-5 offset-lg-1">
+                <ul class="ap-services">
+                    <li><i class="icon_check"></i> 20% Off On Accommodation.</li>
+                    <li><i class="icon_check"></i> 5 star Breakfast</li>
+                    <li><i class="icon_check"></i> 5 Pcs Laundry Per Day</li>
+                    <li><i class="icon_check"></i> Free Wifi with speed up to 1 gb/sec per room.</li>
+                    <li><i class="icon_check"></i> Discount 20% On F&B</li>
+                    <li><i class="icon_check"></i> Gym free to use 24 hour</li>
+                </ul>
+            </div>
         </div>
+    </div>
+    <div class="about-page-services">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="ap-service-item set-bg" data-setbg="img/about/about-p1.jpg">
+                    <div class="api-text">
+                        <h3>Restaurants Services</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="ap-service-item set-bg" data-setbg="img/about/about-p2.jpg">
+                    <div class="api-text">
+                        <h3>Gym & Laundry</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="ap-service-item set-bg" data-setbg="img/about/about-p3.jpg">
+                    <div class="api-text">
+                        <h3>Event & Party</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 </section>
 <!-- About Us Page Section End -->
