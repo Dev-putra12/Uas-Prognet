cc68829f76fef7f7ed597f2606cda0bea05b196b
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 01:50:55 +0800

    Update rooms-check.blade.php

--- resources/views/rooms-check.blade.php
+++ resources/views/rooms-check.blade.php
@@ -26,6 +26,38 @@ include "header.php"
                                 </div>
                             </div>
                         </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px" height="200px">
+                                    </div>
+                                    <div class="col-6">
+                                    <!-- < $tipekamar&$nokamar> -->
+                                        <h5 class="card-title">Reguler Suite #1</h5>
+                                        <p class="card-text">Capacity:	2 person</p>
+                                        <p class="card-text">Services:	Wifi, Television, Bathroom, Dining/Work Table, Wardrobe, Free Breakfast</p>
+                                        <p class="card-text">Price:	    Rp.450.000/Night</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4"> <img src="img/room/room-details.jpg" alt="..." width="400px" height="200px">
+                                    </div>
+                                    <div class="col-6">
+                                    <!-- < $tipekamar&$nokamar> -->
+                                        <h5 class="card-title">Reguler Suite #1</h5>
+                                        <p class="card-text">Capacity:	2 person</p>
+                                        <p class="card-text">Services:	Wifi, Television, Bathroom, Dining/Work Table, Wardrobe, Free Breakfast</p>
+                                        <p class="card-text">Price:	    Rp.450.000/Night</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
