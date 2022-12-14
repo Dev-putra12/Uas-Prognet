4aef35f34744b4af731b1b8ecfa0e0cd27dba50e
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-15 01:32:05 +0800

    Update rooms-check.blade.php

--- resources/views/rooms-check.blade.php
+++ resources/views/rooms-check.blade.php
@@ -11,7 +11,7 @@ include "header.php"
                 <div class="col-lg-10">
                     <div class="ap-title">
                     <div class="card">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="img/room/room-details.jpg" class="card-img-top" alt="...">
 
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
